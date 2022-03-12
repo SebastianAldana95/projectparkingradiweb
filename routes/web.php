fef8874248745469350bad43f8702eac8a2b6095
SebastianAldana95 <brayan95sebas@hotmail.com>
2022-03-12 09:37:49 -0500

    eliminando vistas inncesarias

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::group([
     function () {
 
         Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('dashboard');
-
         Route::resource('vehicles', \App\Http\Controllers\Admin\VehiclesController::class, ['except' => 'show', 'as' => 'admin']);
         Route::resource('users', \App\Http\Controllers\Admin\UsersController::class, ['as' => 'admin']);
 
